1d3d4de06ccf9c80edf355326c94bca30bfc4d5a
Nikolay.Shevyakov <Shevyakovno@gmail.com>
2018-12-10 13:52:18 +0300

    Домашнее задание к 6 уроку

---
+++ 6L_Nikolay.Shevyakov/6L_Nikolay.Shevyakov/Generics.swift
@@ -0,0 +1,9 @@
+//
+//  Generics.swift
+//  6L_Nikolay.Shevyakov
+//
+//  Created by Nikolay Shevyakov on 10.12.2018.
+//  Copyright © 2018 jasno. All rights reserved.
+//
+
+import Foundation
---
+++ 6L_Nikolay.Shevyakov/6L_Nikolay.Shevyakov/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  6L_Nikolay.Shevyakov
+//
+//  Created by Nikolay Shevyakov on 10.12.2018.
+//  Copyright © 2018 jasno. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
